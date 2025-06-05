1753f2a43c9dcc9c53676924753e9713a0cc284d
nizhenha-3 <nizhenhao1793973753@gmail.com>
2025-06-05 15:54:46 +0800

    commit

--- imitation_learning/ACT_Policy/imitate_episode.py
+++ imitation_learning/ACT_Policy/imitate_episode.py
@@ -19,7 +19,7 @@ from policy import ACTPolicy, CNNMLPPolicy
 
 from torch_dataset_all_episode import load_data_all
 
-
+import pandas as pd
 import IPython
 e = IPython.embed
 
@@ -259,6 +259,15 @@ def eval_bc(config, ckpt_name, save_episode=True):
     pred_actions_all = np.array(pred_actions_all)  # [T, 3]
     gt_actions_all = np.array(gt_actions_all)      # [T, 3]
 
+
+    # 保存预测动作
+    pred_df = pd.DataFrame(pred_actions_all, columns=['linear_x', 'linear_y', 'angular_z'])
+    pred_df.to_csv('pred_actions.csv', index=False)
+
+    # 保存真实动作
+    gt_df = pd.DataFrame(gt_actions_all, columns=['linear_x', 'linear_y', 'angular_z'])
+    gt_df.to_csv('gt_actions.csv', index=False)
+
     # 绘图对比
     plt.figure(figsize=(12, 6))
     labels = ['x velocity', 'y velocity', 'z velocity']
@@ -281,152 +290,6 @@ def eval_bc(config, ckpt_name, save_episode=True):
     return
 
 
-# def eval_bc(config, ckpt_name, save_episode=True):
-#     set_seed(1000)
-#     ckpt_dir = config['ckpt_dir']
-#     state_dim = config['state_dim']
-    
-#     policy_class = config['policy_class']
-    
-#     policy_config = config['policy_config']
-    
-#     max_timesteps = config['episode_len']
-    
-#     temporal_agg = config['temporal_agg']
-    
-
-#     # load policy and stats
-#     ckpt_path = os.path.join(ckpt_dir, ckpt_name)
-#     policy = make_policy(policy_class, policy_config)
-#     loading_status = policy.load_state_dict(torch.load(ckpt_path))
-#     print(loading_status)
-
-#     policy.cuda()
-#     policy.eval()
-#     print(f'Loaded: {ckpt_path}')
-#     stats_path = os.path.join(ckpt_dir, f'dataset_stats.pkl')
-#     with open(stats_path, 'rb') as f:
-#         stats = pickle.load(f)
-
-
-#     pre_process = lambda action: (action - stats['action_mean']) / stats['action_std']
-#     post_process = lambda a: a * stats['action_std'] + stats['action_mean']
-
- 
-
-#     query_frequency = policy_config['num_queries']
-#     if temporal_agg:
-#         query_frequency = 1
-#         num_queries = policy_config['num_queries']
-
-#     max_timesteps = int(max_timesteps * 1) # may increase for real-world tasks
-
-#     num_rollouts = 50
-#     episode_returns = []
-#     highest_rewards = []
-#     for rollout_id in range(num_rollouts):
-#         rollout_id += 0
-#         ### set task
-        
-
-#         ### evaluation loop
-#         if temporal_agg:
-#             all_time_actions = torch.zeros([max_timesteps, max_timesteps+num_queries, state_dim]).cuda()
-
-        
-
-#         qpos_history = torch.zeros((1, max_timesteps, state_dim)).cuda()
-#         image_list = [] # for visualization
-#         qpos_list = []
-#         target_qpos_list = []
-#         rewards = []
-
-
-#         with torch.inference_mode():
-#             for t in range(max_timesteps):
-#                 ### update onscreen render and wait for DT
-               
-
-#                 ### process previous timestep to get qpos and image_list
-#                 obs = ts.observation
-#                 if 'images' in obs:
-#                     image_list.append(obs['images'])
-#                 else:
-#                     image_list.append({'main': obs['image']})
-#                 qpos_numpy = np.array(obs['qpos'])
-#                 qpos = pre_process(qpos_numpy)
-#                 qpos = torch.from_numpy(qpos).float().cuda().unsqueeze(0)
-#                 qpos_history[:, t] = qpos
-#                 curr_image = get_image(ts, camera_names)
-
-#                 ### query policy
-#                 if config['policy_class'] == "ACT":
-#                     if t % query_frequency == 0:
-#                         all_actions = policy(qpos, curr_image)
-#                     if temporal_agg:
-#                         all_time_actions[[t], t:t+num_queries] = all_actions
-#                         actions_for_curr_step = all_time_actions[:, t]
-#                         actions_populated = torch.all(actions_for_curr_step != 0, axis=1)
-#                         actions_for_curr_step = actions_for_curr_step[actions_populated]
-#                         k = 0.01
-#                         exp_weights = np.exp(-k * np.arange(len(actions_for_curr_step)))
-#                         exp_weights = exp_weights / exp_weights.sum()
-#                         exp_weights = torch.from_numpy(exp_weights).cuda().unsqueeze(dim=1)
-#                         raw_action = (actions_for_curr_step * exp_weights).sum(dim=0, keepdim=True)
-#                     else:
-#                         raw_action = all_actions[:, t % query_frequency]
-#                 elif config['policy_class'] == "CNNMLP":
-#                     raw_action = policy(qpos, curr_image)
-#                 else:
-#                     raise NotImplementedError
-
-#                 ### post-process actions
-#                 raw_action = raw_action.squeeze(0).cpu().numpy()
-#                 action = post_process(raw_action)
-#                 target_qpos = action
-
-#                 ### step the environment
-#                 ts = env.step(target_qpos)
-
-#                 ### for visualization
-#                 qpos_list.append(qpos_numpy)
-#                 target_qpos_list.append(target_qpos)
-#                 rewards.append(ts.reward)
-
-#             plt.close()
-#         # if real_robot:
-#         #     move_grippers([env.puppet_bot_left, env.puppet_bot_right], [PUPPET_GRIPPER_JOINT_OPEN] * 2, move_time=0.5)  # open
-#         #     pass
-
-#         rewards = np.array(rewards)
-#         episode_return = np.sum(rewards[rewards!=None])
-#         episode_returns.append(episode_return)
-#         episode_highest_reward = np.max(rewards)
-#         highest_rewards.append(episode_highest_reward)
-#         print(f'Rollout {rollout_id}\n{episode_return=}, {episode_highest_reward=}, {env_max_reward=}, Success: {episode_highest_reward==env_max_reward}')
-
-#         # if save_episode:
-#         #     save_videos(image_list, DT, video_path=os.path.join(ckpt_dir, f'video{rollout_id}.mp4'))
-
-#     success_rate = np.mean(np.array(highest_rewards) == env_max_reward)
-#     avg_return = np.mean(episode_returns)
-#     summary_str = f'\nSuccess rate: {success_rate}\nAverage return: {avg_return}\n\n'
-#     for r in range(env_max_reward+1):
-#         more_or_equal_r = (np.array(highest_rewards) >= r).sum()
-#         more_or_equal_r_rate = more_or_equal_r / num_rollouts
-#         summary_str += f'Reward >= {r}: {more_or_equal_r}/{num_rollouts} = {more_or_equal_r_rate*100}%\n'
-
-#     print(summary_str)
-
-#     # save success rate to txt
-#     result_file_name = 'result_' + ckpt_name.split('.')[0] + '.txt'
-#     with open(os.path.join(ckpt_dir, result_file_name), 'w') as f:
-#         f.write(summary_str)
-#         f.write(repr(episode_returns))
-#         f.write('\n\n')
-#         f.write(repr(highest_rewards))
-
-#     return success_rate, avg_return
 
 
 def forward_pass(data, policy):
